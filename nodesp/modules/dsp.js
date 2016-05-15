9cec17279f065c73b8ddc2bbd3e6e0d35141fec6
Ahmed Agiza <ahmed.agiza22@gmail.com>
2016-05-15 20:53:34 +0200

    Project Initialization

---
+++ nodesp/modules/dsp.js
@@ -0,0 +1,14 @@
+module.exports = {
+    DFT: function(signal) {
+
+    },
+    LowPass: function (signal) {
+
+    },
+    HighPass: function (signal) {
+
+    },
+    BandPass: function (signal) {
+
+    }
+}
