997d2bd70b82696d2b7e6edc41cccf042c1e0194
Ahmed Agiza <ahmed.agiza22@gmail.com>
2016-05-20 19:40:48 +0200

    FFT and InverseFFT

--- nodesp/modules/dsp.js
+++ nodesp/modules/dsp.js
@@ -1,5 +1,77 @@
+var Complex = require('Complex');
+
 module.exports = {
-    DFT: function(signal) {
+    FFT: function(complexSignal, cb) {
+      var PI = 3.14159265358979323846264;
+
+      var numSamples = Object.keys(complexSignal).length;
+      if(numSamples & (numSamples -1)) {
+        var target = Math.pow(2, new Number(numSamples).toString(2).length);
+        for (var i = 0; i < target - numSamples; i++) {
+          complexSignal.push(new Complex(0, 0));
+        }
+        numSamples = Object.keys(complexSignal).length;
+      }
+
+
+      var _fft = function(samples) {
+        var size = samples.length;
+
+        if (size == 1)
+          return samples;
+
+        var even = [], odd = [];
+
+        for (var i = 0; i < samples.length; i += 2) {
+          even.push(samples[i]);
+          odd.push(samples[i + 1]);
+        }
+
+
+
+
+
+        even = _fft(even);
+        odd = _fft(odd);
+
+
+        for(var i = 0; i < size / 2; i++) {
+          var theta = -2 * PI * i / size;
+          var xEven = even[i];
+          var xOdd = new Complex(Math.cos(theta), Math.sin(theta)).multiply(odd[i]);
+
+          samples[i] = new Complex(xEven.real, xEven.im).add(xOdd);
+          samples[i + size / 2] = new Complex(xEven.real, xEven.im).subtract(xOdd);
+        }
+
+
+
+        return samples;
+
+      }
+
+      var fftResults = _fft(complexSignal);
+
+      cb(null, fftResults);
+
+    },
+    InverseFFT: function(signal, cb) {
+      for (var i = 0; i < signal.length; i++)
+        signal[i].conjugate();
+
+
+
+      signal = this.FFT(signal, function(err, results) {
+        if (err)
+          return cb(err);
+
+        for (var i = 0; i < results.length; i++) {
+          results[i].conjugate();
+          results[i].divide(signal.length);
+        }
+
+        cb(null, results);
+      });
 
     },
     LowPass: function (signal) {
--- nodesp/routes/index.js
+++ nodesp/routes/index.js
@@ -2,8 +2,9 @@ var express = require('express');
 var router = express.Router();
 var path = require('path');
 var fs = require('fs');
+var Complex = require('Complex');
+var DSP = require('../modules/dsp');
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'NodeSP' });
 });
@@ -13,10 +14,58 @@ router.post('/fft', function(req, res, next) {
   var filter = req.body.filter;
   var filePath = path.join(process.cwd(), req.files.audio.path);
   console.log('-----------');
-  console.log('PROCESSING GOES HERE');
+  var ndarrayWav = require('ndarray-wav');
+  ndarrayWav.open(filePath, function (err, chunkMap, chunkArr) {
+    if (err) {
+      console.error(err);
+      fs.unlink(filePath);
+      return res.status(500).json(err);
+    }
+
+    var format = chunkMap.fmt;
+    var ndSamples = chunkMap.data;
+    var sampleData = ndSamples.data;
+    var numChannels = ndSamples.shape[0];
+    var numSamples = ndSamples.shape[1];
+    var formatData = chunkArr[0].data;
+
+
+
+
+    var complexSignal = [];
+    for(var key in sampleData) {
+      complexSignal.push(new Complex(sampleData[key], 0));
+    }
+    DSP.FFT(complexSignal, function (err, result) {
+      fs.unlink(filePath);
+      if (err) {
+        console.error(err);
+        return res.status(500).json(err);
+      }
+
+      /*
+      * Wirte Audio File
+      *
+      *DSP.InverseFFT(result, function(err, inv) {
+        ndSamples.data = {};
+        var index = 0;
+        for (var i = 0; i < inv.length; i++) {
+          ndSamples.data[i] = inv[i].real;
+        }
+
+
+        ndarrayWav.write('output.wav', ndSamples, formatData, function (err) {if (err) console.error(err);});
+      })
+      */
+
+
+      res.render('fft', { title: 'NodeSP', fft:  result});
+    });
+
+
+  });
   console.log('-----------');
-  fs.unlink(filePath);
-  res.render('fft', { title: 'NodeSP' });
+
 });
 
 router.post('/lpf', function(req, res, next) {
